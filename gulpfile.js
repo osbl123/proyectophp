34f53b3756d528c2aecb88104123b3fb46e7d6d4
osmarB <osmarbl26@gmil.com>
2018-06-15 19:26:17 -0400

    modificado para correr sass y estructura sugerida del proyecto

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass',()=> {
+	return gulp.src([
+		'node_modules/bootstrap/scss/bootstrap.scss',
+		'plantilla/scss/*.scss'
+	])
+	.pipe(sass({
+		outputStyle:'expanded'
+	}))
+	.pipe(gulp.dest('public/css'));
+})
+
+gulp.task('checksass',['sass'],()=> {
+	gulp.watch([
+		'node_modules/bootstrap/scss/bootstrap.scss',
+		'plantilla/scss/*.scss'
+	],['sass']);
+})
+
+gulp.task('js',()=> {
+	return gulp.src([
+		'plantilla/js/*.js'
+	])
+	.pipe(gulp.dest('public/js'));
+})
+
+gulp.task('checkjs',['js'],()=> {
+	gulp.watch([
+		'plantilla/js/*.js'
+	],['js']);
+})
+
+gulp.task('libsjs',()=> {
+	return gulp.src([
+		'node_modules/bootstrap/dist/js/bootstrap.min.js',
+		'node_modules/jquery/dist/jquery.min.js',
+		'node_modules/popper.js/dist/umd/popper.min.js'
+	])
+	.pipe(gulp.dest('public/js'));
+})
+
+
+gulp.task('font-awesome', ()=> {
+	return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+		.pipe(gulp.dest('public/css'));
+})
+
+gulp.task('fonts',()=>{
+	return gulp.src('node_modules/font-awesome/fonts/*')
+		.pipe(gulp.dest('public/fonts'));
+})
+
+gulp.task('default',['libsjs','checksass','checkjs','font-awesome','fonts']);
